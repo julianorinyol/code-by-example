acde2694a9b55f9f141fae5f4fd400063095560c
JulianOrinyol <julianorinyol@gmail.com>
2015-10-23 16:19:06 -0700

    using the path property on response to create links

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,19 +3,18 @@ require "net/http"
 class SearchController < ApplicationController
   def search
     # https://api.github.com/search/code?q=describe+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:_controller_spec.rb
-    urls = [
-      {url:"https://api.github.com/search/code?q=describe+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:_controller_spec.rb", path: "/tree/master/spec/controllers"},
-      {url:"https://api.github.com/search/code?q=factory+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:factories.rb", path: "/spec/factories.rb"}
-    ]
+    
+    urls = ["https://api.github.com/search/code?q=describe+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:_controller_spec.rb", "https://api.github.com/search/code?q=factory+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:factories.rb"]
+    
     index = params[:q].to_i
-    resp = Net::HTTP.get(URI.parse(urls[index][:url]))
+    resp = Net::HTTP.get(URI.parse(urls[index]))
     data = JSON.parse(resp)
     total_count = data["total_count"]
     items = data["items"]
 
     @repos = []
     items.each do |item|
-      path = "http://github.com/" + item["repository"]["full_name"] + urls[index][:path]
+      path = "http://github.com/" + item["repository"]["full_name"] + "/tree/master/" + item["path"]
       @repos << path unless @repos.include? path 
     end
   end
