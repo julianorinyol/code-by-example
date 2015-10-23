4757a07069378ceba2d5aa48dfc710bb63cf5d40
JulianOrinyol <julianorinyol@gmail.com>
2015-10-23 15:43:58 -0700

    made select tag that submits search to github and shows links to repos

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,16 +3,23 @@ require "net/http"
 class SearchController < ApplicationController
   def search
     # https://api.github.com/search/code?q=describe+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:_controller_spec.rb
-    url = "https://api.github.com/search/code?q=describe+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:_controller_spec.rb"
-    resp = Net::HTTP.get(URI.parse(url))
+    urls = [
+      {url:"https://api.github.com/search/code?q=describe+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:_controller_spec.rb", path: "/tree/master/spec/controllers"},
+      {url:"https://api.github.com/search/code?q=factory+repo:gitlabhq/gitlabhq+repo:sharetribe/sharetribe+language:ruby+path:spec+filename:factories.rb", path: "/spec/factories.rb"}
+    ]
+    index = params[:q].to_i
+    resp = Net::HTTP.get(URI.parse(urls[index][:url]))
     data = JSON.parse(resp)
     total_count = data["total_count"]
     items = data["items"]
 
     @repos = []
     items.each do |item|
-      name = item["repository"]["full_name"]
-      @repos << name unless @repos.include? name 
+      path = "http://github.com/" + item["repository"]["full_name"] + urls[index][:path]
+      @repos << path unless @repos.include? path 
     end
   end
 end
+
+# 1 github.com/githubq/githubq/spec/controllers
+# 2 github.com/githubq/githubq/spec/factories.rb
